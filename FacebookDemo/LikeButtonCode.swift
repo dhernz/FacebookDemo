b0b1f9298c00f94788ec323a44ab039d7d0ea19a
Doris Hernandez <d.hernz@gmail.com>
2016-10-13 21:33:35 -0700

    Initial Commit

---
+++ FacebookDemo/LikeButtonCode.swift
@@ -0,0 +1,35 @@
+//
+//  LikeButtonCode.swift
+//  FacebookDemo
+//
+//  Created by Doris Hernandez on 10/13/16.
+//  Copyright © 2016 Doris Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class LikeButtonCode: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FacebookDemo/ScrollViewCode.swift
@@ -0,0 +1,40 @@
+//
+//  ScrollViewCode.swift
+//  FacebookDemo
+//
+//  Created by Doris Hernandez on 10/13/16.
+//  Copyright © 2016 Doris Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewCode: UIViewController {
+
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var feedImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.contentSize = feedImageView.frame.size
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
